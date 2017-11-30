4bf815984e752d3376865293e0f6c716f1efcf6b
Tomvh123 <thaaster@avans.nl>
2017-11-30 21:05:51 +0100

    Added characters

--- model/serie.js
+++ model/serie.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const CharacterSchema = require('./characters');
+
+
+
 
 const SerieSchema = new Schema({
    name: {
@@ -11,10 +15,11 @@ const SerieSchema = new Schema({
         required: true
     },
     imagePath: String,
-    character: [],
+    characters: [CharacterSchema],
     creators: []
 
 });
+//console.log(CharacterSchema);
 
 const Serie = mongoose.model('serie', SerieSchema);
 
@@ -28,10 +33,20 @@ Serie.count({}, function (err, count) {
             'He returns five years later a changed man,' +
             'determined to clean up the city as a hooded vigilante armed with a bow.',
             imagePath: 'http://www.imdb.com/title/tt2193021/mediaviewer/rm1906844672',
-            character: ['nothing1', 'nothing2'],
-            creators: ['nothing1', 'nothing2']
+            characters: [
+                {
+                    name: 'The Green Arrow',
+                    description: 'Hes green and has a bow and arrow',
+                    actor: []
+                }
+            ],
+
+            creators: ['nothing1', 'nothing2'],
+
         }).save();
+
     }
 });
+
 
 module.exports = Serie;
