964f511b30213eb1c2da55e0e4e838d40468fa0d
Tomvh123 <thaaster@avans.nl>
2017-12-01 15:20:47 +0100

    meta data veranderd met werkende imagePaths

--- model/serie.js
+++ model/serie.js
@@ -23,7 +23,7 @@ const Serie = mongoose.model('serie', SerieSchema);
 
 
 Serie.count({}, function (err, count) {
-    if(count < 2) {
+    if(count < 1) {
         console.log('serie toevoegen...')
 
         const serie = new Serie({
@@ -32,7 +32,7 @@ Serie.count({}, function (err, count) {
             'is missing and presumed dead when his yacht is lost at sea. ' +
             'He returns five years later a changed man,' +
             'determined to clean up the city as a hooded vigilante armed with a bow.',
-            imagePath: 'http://www.imdb.com/title/tt2193021/mediaviewer/rm1906844672',
+            imagePath: 'https://d2kmm3vx031a1h.cloudfront.net/FOovdnm5RpayhDQrcMQk_p14238959_b_v8_ab.jpg',
             characters: [
                 {
                     name: 'The Green Arrow',
@@ -44,25 +44,44 @@ Serie.count({}, function (err, count) {
             creators: ['nothing1', 'nothing2'],
 
         });
+        const serie2 = new Serie({
+            name: 'The Flash' + count,
+            description:'After being struck by lightning, Barry Allen wakes up from his coma to discover ' +
+            'hes been given the power of super speed, ' +
+            'becoming the Flash, fighting crime in Central City.',
+            imagePath: 'https://www.bleedingcool.com/wp-content/uploads/2017/09/Flash.png',
+            characters: [
+                {
+                    name: 'The Flash',
+                    description: 'Hes the fastest man alive',
+                    actors: []
+                }
+            ],
+
+            creators: ['nothing1', 'nothing2'],
+
+        });
         const actor = new Actor({
 
-            name: 'Stephen Amell ' + count,
-            description: 'Actor on Arrow',
-            imagePath: 'http.dsjfakdjf.png'
+            name: 'Person ' + count,
+            description: 'Actor',
+            imagePath: 'image'
         });
 
         const actor2 = new Actor({
 
             name: 'Stephen Amell2- ' + count,
             description: 'Actor on Arrow',
-            imagePath: 'http.dsjfakdjf.png'
+            imagePath: 'image'
         });
         serie.characters[0].actors.push(actor);
-        serie.characters[0].actors.push(actor);
         serie.characters[0].actors.push(actor2);
+        serie2.characters[0].actors.push(actor);
+        serie2.characters[0].actors.push(actor2);
         actor.save();
         actor2.save();
         serie.save();
+        serie2.save();
 
     }
 });
