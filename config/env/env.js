738e35d8d1c96f7f14e62dd17b8485f2f89fc020
tomvh123 <thaaster@avans.nl>
2017-12-11 20:59:43 +0100

    Heroku

--- config/env/env.js
+++ config/env/env.js
@@ -5,7 +5,7 @@ var env = {
     webPort: process.env.PORT || 3000,
     dbHost: process.env.DB_HOST || 'localhost',
     dbPort: process.env.DB_PORT || '',
-    dbUser: process.env.DB_USES || '',
+    dbUser: process.env.DB_USER || '',
     dbPassword: process.env.DB_PASSWORD || '',
     dbDatabase: process.env.DB_DATABASE || 'serie'
 };
